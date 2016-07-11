3526c95a74f96cba912b74a151b5939aaebc7956
WesselKoorn <wesselkoorn@icloud.com>
2016-07-11 22:51:41 +0200

    Add Excel functionality to model

---
+++ Blycolin Print and Mail/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Packaging;
+
+
+namespace Blycolin_Print_and_Mail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string docName = @"testSheet.xlsx";
+        private const string sheetName = "Blad1";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Model.InsertText(docName, sheetName, "B", 6, "Tekst vanuit programma");
+        }
+    }
+}
---
+++ Blycolin Print and Mail/Model.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.IO;
+using System.Windows;
+
+namespace Blycolin_Print_and_Mail
+{
+    class Model
+    {
+        // Insert text to a cell, given a document name, sheet name, column name and row number.
+        public static void InsertText(string docName, string sheetName, string colName, uint rowIndex, string text)
+        {
+            try
+            {
+                // Open the document for editing.
+                using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Open(docName, true))
+                {
+                    // Get the SharedStringTablePart. If it does not exist, create a new one.
+                    SharedStringTablePart shareStringPart;
+                    if (spreadSheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>().Count() > 0)
+                    {
+                        shareStringPart = spreadSheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                    }
+                    else
+                    {
+                        shareStringPart = spreadSheet.WorkbookPart.AddNewPart<SharedStringTablePart>();
+                    }
+
+                    // Insert the text into the SharedStringTablePart.
+                    int index = InsertSharedStringItem(text, shareStringPart);
+
+                    // Get worksheet
+                    IEnumerable<Sheet> sheets = spreadSheet.WorkbookPart.Workbook.GetFirstChild<Sheets>().Elements<Sheet>().Where(s => s.Name == sheetName);
+                    if (sheets.Count() == 0)
+                    {
+                        // The specified worksheet does not exist.
+                        MessageBox.Show("Kan het Excel blad niet vinden.");
+                        return;
+                    }
+                    string relationshipId = sheets.First().Id.Value;
+                    WorksheetPart worksheetPart = (WorksheetPart)spreadSheet.WorkbookPart.GetPartById(relationshipId);
+
+                    // Insert the cell into the worksheet.
+                    Cell cell = InsertCellInWorksheet(colName, rowIndex, worksheetPart);
+
+                    // Set the value of the cell.
+                    cell.CellValue = new CellValue(index.ToString());
+                    cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+
+                    // Save the new worksheet.
+                    worksheetPart.Worksheet.Save();
+
+                    MessageBox.Show("Bestand is opgeslagen.");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Oops!");
+            }
+        }
+
+        // Given text and a SharedStringTablePart, creates a SharedStringItem with the specified text 
+        // and inserts it into the SharedStringTablePart. If the item already exists, returns its index.
+        private static int InsertSharedStringItem(string text, SharedStringTablePart shareStringPart)
+        {
+            // If the part does not contain a SharedStringTable, create one.
+            if (shareStringPart.SharedStringTable == null)
+            {
+                shareStringPart.SharedStringTable = new SharedStringTable();
+            }
+
+            int i = 0;
+
+            // Iterate through all the items in the SharedStringTable. If the text already exists, return its index.
+            foreach (SharedStringItem item in shareStringPart.SharedStringTable.Elements<SharedStringItem>())
+            {
+                if (item.InnerText == text)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+
+            // The text does not exist in the part. Create the SharedStringItem and return its index.
+            shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
+            shareStringPart.SharedStringTable.Save();
+
+            return i;
+        }
+
+        // Given a column name, a row index, and a WorksheetPart, inserts a cell into the worksheet. 
+        // If the cell already exists, returns it. 
+        private static Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            Worksheet worksheet = worksheetPart.Worksheet;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            string cellReference = columnName + rowIndex;
+
+            // If the worksheet does not contain a row with the specified row index, insert one.
+            Row row;
+            if (sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).Count() != 0)
+            {
+                row = sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
+            }
+            else
+            {
+                row = new Row() { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+
+            // If there is not a cell with the specified column name, insert one.  
+            if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0)
+            {
+                return row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+            }
+            else
+            {
+                // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
+                Cell refCell = null;
+                foreach (Cell cell in row.Elements<Cell>())
+                {
+                    if (string.Compare(cell.CellReference.Value, cellReference, true) > 0)
+                    {
+                        refCell = cell;
+                        break;
+                    }
+                }
+
+                Cell newCell = new Cell() { CellReference = cellReference };
+                row.InsertBefore(newCell, refCell);
+
+                worksheet.Save();
+                return newCell;
+            }
+        }
+    }
+}
